29efbecdd37ba046c4429283c173c1e7b14544da
rankware <rankwareagency@gmail.com>
2025-05-16 10:49:32 +0530

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-tailwind/react";
 import {
   Instagram,
   Linkedin,
@@ -33,13 +34,13 @@ export const ContactSection = () => {
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Get In <span className="text-primary"> Touch</span>
+          Get In <span className="text-primary text-glow"> Touch</span>
         </h2>
 
-        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+        {/*<p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
           Have a project in mind or want to collaborate? Feel free to reach out.
           I'm always open to discussing new opportunities.
-        </p>
+        </p>*/}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           <div className="space-y-8">
@@ -59,7 +60,7 @@ export const ContactSection = () => {
                     href="mailto:hello@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    hello@gmail.com
+                    rankwareagency@gmail.com
                   </a>
                 </div>
               </div>
@@ -73,11 +74,11 @@ export const ContactSection = () => {
                     href="tel:+11234567890"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    +1 (123) 456-7890
+                    +91 9391100961
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
+              {/*<div className="flex items-start space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
                   <MapPin className="h-6 w-6 text-primary" />{" "}
                 </div>
@@ -87,16 +88,36 @@ export const ContactSection = () => {
                     Vancouver, BC, Canada
                   </a>
                 </div>
-              </div>
+              </div>*/}
             </div>
 
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
-                  <Linkedin />
-                </a>
-                <a href="#" target="_blank">
+                {/*<a
+                  href="https://cal.com/rankware-eopgp9"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button
+                    className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform hover:scale-105 transition duration-300 ease-in-out ring-2 ring-amber-300 hover:ring-amber-500"
+                    size="lg"
+                  >
+                    Schedule a Quick Call!
+                  </Button>
+                </a>*/}
+                <div className="pt-4 opacity-0 animate-fade-in-delay-4 ">
+                  <a
+                    href="https://cal.com/rankware-eopgp9"
+                    className="cosmic-button w-full flex items-center justify-center glow-borderr gap-2 bg-[#1e3a8a] hover:bg-[#1e40af] text-white font-semibold py-3 px-6 rounded-4xl transition duration-300 shadow-md hover:shadow-lg"
+                  >
+                    Schedule a Quick Call!
+                  </a>
+                  <div className="text-xl md:text-xl text-muted-foreground text-glow max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3 pt-1">
+                    <span className="">It's free</span>
+                  </div>
+                </div>
+                {/*<a href="#" target="_blank">
                   <Twitter />
                 </a>
                 <a href="#" target="_blank">
@@ -104,24 +125,24 @@ export const ContactSection = () => {
                 </a>
                 <a href="#" target="_blank">
                   <Twitch />
-                </a>
+                </a>*/}
               </div>
             </div>
           </div>
 
-          <div
-            className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
-          >
-            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+          <div className="bg-card p-8 rounded-lg shadow-xs">
+            <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
 
-            <form className="space-y-6">
+            <form
+              action="https://formspree.io/f/mbloqwqd"
+              method="POST"
+              className="space-y-6"
+            >
               <div>
                 <label
                   htmlFor="name"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Your Name
                 </label>
                 <input
@@ -129,7 +150,7 @@ export const ContactSection = () => {
                   id="name"
                   name="name"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                   placeholder="Pedro Machado..."
                 />
               </div>
@@ -139,7 +160,6 @@ export const ContactSection = () => {
                   htmlFor="email"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Your Email
                 </label>
                 <input
@@ -147,7 +167,7 @@ export const ContactSection = () => {
                   id="email"
                   name="email"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                   placeholder="john@gmail.com"
                 />
               </div>
@@ -157,26 +177,22 @@ export const ContactSection = () => {
                   htmlFor="message"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Your Message
                 </label>
                 <textarea
                   id="message"
                   name="message"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-none"
                   placeholder="Hello, I'd like to talk about..."
                 />
               </div>
 
               <button
                 type="submit"
-                disabled={isSubmitting}
-                className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
-                )}
+                className="cosmic-button w-full flex items-center justify-center gap-2 bg-[#1e3a8a] hover:bg-[#1e40af] text-white font-semibold py-3 px-6 rounded-md glow-borderr transition duration-300 shadow-md hover:shadow-lg"
               >
-                {isSubmitting ? "Sending..." : "Send Message"}
+                Send Message
                 <Send size={16} />
               </button>
             </form>
