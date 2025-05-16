3b4ac791e41510fc6a4b5e45c5537b14f2b74367
rankware <rankwareagency@gmail.com>
2025-05-16 10:50:29 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,28 +8,30 @@ export const HeroSection = () => {
     >
       <div className="container max-w-4xl mx-auto text-center z-10">
         <div className="space-y-6">
-          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-            <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
-            <span className="text-primary opacity-0 animate-fade-in-delay-1">
-              {" "}
-              Pedro
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight space-y-2">
+            <span className="block opacity-0 text-glow animate-fade-in">
+              We Don’t Follow the Future -
             </span>
-            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
-              {" "}
-              Machado
+            <span className="block text-primary text-glow opacity-0 animate-fade-in-delay-1">
+              We Build It.
             </span>
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            I create stellar web experiences with modern technologies.
-            Specializing in front-end development, I build interfaces that are
-            both beautiful and functional.
+            AI Strategy · Autonomous Agents · Enterprise Consulting · Smart
+            Chatbots
           </p>
 
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-            <a href="#projects" className="cosmic-button">
-              View My Work
+            <a
+              href="https://cal.com/rankware-eopgp9"
+              className="cosmic-button bg-[#1e3a8a] hover:bg-[#1e40af] glow-borderr text-white font-semibold py-3 px-6 rounded-4xl transition duration-300 shadow-md hover:shadow-lg"
+            >
+              Schedule a Quick Call!
             </a>
+            <div className="text-lg md:text-xl text-muted-foreground text-glow max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3 pt-4">
+              <span className="">It's free</span>
+            </div>
           </div>
         </div>
       </div>
